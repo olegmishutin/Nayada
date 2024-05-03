7beb7c2a325ef43d8bad0fa84c9bad45d66cf863
OlegMishutin <omisutin646@gmail.com>
2024-05-03 15:35:57 +0300

    fixed

--- server/product/models.py
+++ server/product/models.py
@@ -1,3 +1,4 @@
+import os
 from django.db import models
 
 
@@ -14,18 +15,34 @@ class Product(models.Model):
         verbose_name = 'Продукт'
         verbose_name_plural = 'Продукты'
 
+    def delete(self, using=None, keep_parents=False):
+        for photo in self.photos.all():
+            photo.delete()
+
+        return super(Product, self).delete(using=using, keep_parents=keep_parents)
+
     def __str__(self):
         return self.name
 
 
+def save_product_photo(instance, filename):
+    return f'product_{instance.product.id}_photos/{filename}'
+
+
 class ProductPhoto(models.Model):
     product = models.ForeignKey(Product, related_name='photos', on_delete=models.CASCADE)
-    photo = models.ImageField(upload_to='product_photoes')
+    photo = models.ImageField(upload_to=save_product_photo)
 
     class Meta:
         db_table = 'ProductPhoto'
         verbose_name = 'Фото продукции'
         verbose_name_plural = 'Фотографии продукции'
+
+    def delete(self, using=None, keep_parents=False):
+        if self.photo and os.path.exists(self.photo.path):
+            os.remove(self.photo.path)
+
+        return super(ProductPhoto, self).delete(using=using, keep_parents=keep_parents)
 
     def __str__(self):
         return self.product.name
